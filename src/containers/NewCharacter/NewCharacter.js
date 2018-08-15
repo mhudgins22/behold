91bf6d7cb0cee7640c1a6d916b47c8a7707eee77
Blackout506 <blackout@Matthews-MacBook-Pro.local>
2018-08-14 23:29:29 -0400

    Began making NewCharacter form inputs dynamically

--- src/containers/NewCharacter/NewCharacter.js
+++ src/containers/NewCharacter/NewCharacter.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
 
+import Input from '../../components/UI/Input/Input';
+
 class NewCharacter extends Component {
+  state = {
+    controls: {
+      characterName: {
+        elementType: "input",
+        elementConfig: {
+          type: "input",
+          placeholder: "Your Character's Name"
+        },
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
+      },
+
+    }
+  }
+
   render() {
-    let form = (
-      <form>
-        <input className={null} type="text" name="characterName" placeholder="Character Name" />
-        <input className={null} type="select" name="race" placeholder="Race" />
-        <input className={null} type="select" name="class" placeholder="Class" />
-      </form>
-    );
+    let formElements = [];
+    for (let element in this.state.controls) {
+      formElements.push({
+        id: element,
+        config: this.state.controls[element]
+      });
+    }
+
+    let form = formElements.map(element => (
+      <Input
+        key = {element.id}
+        elementType = {element.config.elementType}
+        elementConfig = {element.config.elementConfig}
+        value = {element.config.value}
+        validation = {element.config.validation}
+        placeholder = {element.config.placeholder}
+        valid = {element.config.valid}
+        touched = {element.config.touched}
+        shouldValidate = {null}
+        changed = {null}
+        />
+    ));
 
     return (
       <div>
