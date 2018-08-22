f2dc4eb4c5564c9d5a4adf3f5156251e753c0f0b
unknown <mikejhudgins@gmail.com>
2018-08-22 16:24:48 -0400

    Made navbar sticky instead

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 
 import Layout from "./containers/Layout/Layout";
 
-import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
+			<div>
 				<Layout />
 			</div>
 		);
