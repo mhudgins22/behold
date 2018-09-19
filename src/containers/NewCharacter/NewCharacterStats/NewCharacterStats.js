420bdf56238c999ec63a2dbd2171c417f7edb35c
Blackout506 <blackout@Matthews-MacBook-Pro.local>
2018-09-19 17:21:20 -0400

    Create StatsSuggestionBox component. Clean up code in NewCharacterStats

--- src/containers/NewCharacter/NewCharacterStats/NewCharacterStats.js
+++ src/containers/NewCharacter/NewCharacterStats/NewCharacterStats.js
@@ -6,6 +6,7 @@ import Button from '../../../components/UI/Button/Button';
 import Input from '../../../components/UI/Input/Input';
 import classes from './NewCharacterStats.css';
 import * as actions from '../../../store/actions/index';
+import StatsSuggestionBox from './StatsSuggestionBox/StatsSuggestionBox';
 
 class NewCharacterStats extends Component {
   state = {
@@ -530,97 +531,6 @@ class NewCharacterStats extends Component {
 
   render () {
 
-    let firstSuggestion = "";
-    let secondSuggestion = "";
-    let additionalNotes = "";
-    let raceBonusInfo = "";
-
-    switch (this.props.class) {
-      case "Barbarian":
-        firstSuggestion = "Strength";
-        secondSuggestion = "Constitution";
-        break;
-      case "Bard":
-        firstSuggestion = "Charisma";
-        secondSuggestion = "Dexterity";
-        break;
-      case "Cleric":
-        firstSuggestion = "Wisdom";
-        secondSuggestion = "Strength or Constitution";
-        break;
-      case "Druid":
-        firstSuggestion = "Wisdom";
-        secondSuggestion = "Constitution";
-        break;
-      case "Fighter":
-        firstSuggestion = "Strength or Dexterity";
-        secondSuggestion = "Constitution";
-        additionalNotes = "Your choice of strength or dexterity should depend on whether you would like to focus of melee weapons (strength) or ranged weapons (dexterity)";
-        break;
-      case "Monk":
-        firstSuggestion = "Dexterity";
-        secondSuggestion = "Wisdom";
-        break;
-      case "Paladin":
-        firstSuggestion = "Strength";
-        secondSuggestion = "Charisma";
-        break;
-      case "Ranger":
-        firstSuggestion = "Dexterity";
-        secondSuggestion = "Wisdom";
-        break;
-      case "Rouge":
-        firstSuggestion = "Dexterity";
-        secondSuggestion = "Intelligence";
-        additionalNotes = "Choose charisma instead of intelligence if you plan to emphasize deception and social interaction";
-        break;
-      case "Sorcerer":
-        firstSuggestion = "Charisma";
-        secondSuggestion = "Constitution";
-        // additionalNotes = "Suggested cantrips: light, prestidigitation, ray of frost, and shocking grasp. Suggested first level spells: shield and magic missile.";
-        break;
-      case "Warlock":
-        firstSuggestion = "Charisma";
-        secondSuggestion = "Constitution";
-        // additionalNotes = "Suggested cantrips: blast and chill touch. Suggested first level spells: charm person and witch bolt.";
-        break;
-      case "Wizard":
-        firstSuggestion = "Intelligence";
-        secondSuggestion = "Constitution or Dexterity";
-        // additionalNotes = "Suggested cantrips: mage hand, light, and ray of frost. Suggested first level spells for your spellbook: burning hands, charm person, feather fall, mage armor, magic missile, and sleep.";
-        break;
-    };
-
-    switch (this.props.race) {
-      case "Dragonborn":
-        raceBonusInfo = "+2 Strength and +1 Charisma";
-        break;
-      case "Dwarf":
-        raceBonusInfo = "+2 Constitution";
-        break;
-      case "Elf":
-        raceBonusInfo = "+2 Dexterity";
-        break;
-      case "Gnome":
-        raceBonusInfo = "+2 Intelligence";
-        break;
-      case "Half-Elf":
-        raceBonusInfo = "+2 Charisma and +1 to Two Other Ability Scores of Your Choice";
-        break;
-      case "Halfling":
-        raceBonusInfo = "+2 Dexterity";
-        break;
-      case "Half-Orc":
-        raceBonusInfo = "+2 Strength and +1 Constitution";
-        break;
-      case "Human":
-        raceBonusInfo = "+1 to All Ability Scores";
-        break;
-      case "Tiefling":
-        raceBonusInfo = "+2 Charisma and +1 Intelligence";
-        break;
-    };
-
     const directions = (
       <div>
         <h1>Roll Your Stats</h1>
@@ -635,14 +545,6 @@ class NewCharacterStats extends Component {
           <li>Complete steps 1 - 3 six times until you have your six stat values.</li>
           <li>Choose which stat values you would like to represent each of the attributes.</li>
         </ol>
-      </div>
-    );
-
-    let suggestionBox = (
-      <div>
-        <p>As a {this.props.class}, your suggested primary ability score is {firstSuggestion}, followed by {secondSuggestion}.</p>
-        <p>{additionalNotes}</p>
-        <p>As a {this.props.race}, you will receive {raceBonusInfo}.</p>
       </div>
     );
 
@@ -680,7 +582,7 @@ class NewCharacterStats extends Component {
           text="Roll My Stats" />
         <h3>Your automatically rolled stat values are: {this.displayStatsNicely(this.props.stats)}</h3>
         <br />
-        {suggestionBox}
+        <StatsSuggestionBox class={this.props.class} race={this.props.race}/>
         <br />
         <h3>Enter your values below:</h3>
         {form}
---
+++ src/containers/NewCharacter/NewCharacterStats/StatsSuggestionBox/StatsSuggestionBox.js
@@ -0,0 +1,105 @@
+import React from 'react';
+
+import classes from '../../NewCharacter.css';
+
+const StatsSuggestionBox = (props) => {
+  let firstSuggestion = "";
+  let secondSuggestion = "";
+  // let additionalNotes = "";
+  let raceBonusInfo = "";
+
+  switch (props.class) {
+    case "Barbarian":
+      firstSuggestion = "Strength";
+      secondSuggestion = "Constitution";
+      break;
+    case "Bard":
+      firstSuggestion = "Charisma";
+      secondSuggestion = "Dexterity";
+      break;
+    case "Cleric":
+      firstSuggestion = "Wisdom";
+      secondSuggestion = "Strength or Constitution";
+      break;
+    case "Druid":
+      firstSuggestion = "Wisdom";
+      secondSuggestion = "Constitution";
+      break;
+    case "Fighter":
+      firstSuggestion = "Strength or Dexterity";
+      secondSuggestion = "Constitution";
+      // additionalNotes = "Your choice of strength or dexterity should depend on whether you would like to focus of melee weapons (strength) or ranged weapons (dexterity)";
+      break;
+    case "Monk":
+      firstSuggestion = "Dexterity";
+      secondSuggestion = "Wisdom";
+      break;
+    case "Paladin":
+      firstSuggestion = "Strength";
+      secondSuggestion = "Charisma";
+      break;
+    case "Ranger":
+      firstSuggestion = "Dexterity";
+      secondSuggestion = "Wisdom";
+      break;
+    case "Rouge":
+      firstSuggestion = "Dexterity";
+      secondSuggestion = "Intelligence";
+      // additionalNotes = "Choose charisma instead of intelligence if you plan to emphasize deception and social interaction";
+      break;
+    case "Sorcerer":
+      firstSuggestion = "Charisma";
+      secondSuggestion = "Constitution";
+      // additionalNotes = "Suggested cantrips: light, prestidigitation, ray of frost, and shocking grasp. Suggested first level spells: shield and magic missile.";
+      break;
+    case "Warlock":
+      firstSuggestion = "Charisma";
+      secondSuggestion = "Constitution";
+      // additionalNotes = "Suggested cantrips: blast and chill touch. Suggested first level spells: charm person and witch bolt.";
+      break;
+    case "Wizard":
+      firstSuggestion = "Intelligence";
+      secondSuggestion = "Constitution or Dexterity";
+      // additionalNotes = "Suggested cantrips: mage hand, light, and ray of frost. Suggested first level spells for your spellbook: burning hands, charm person, feather fall, mage armor, magic missile, and sleep.";
+      break;
+  };
+
+  switch (props.race) {
+    case "Dragonborn":
+      raceBonusInfo = "+2 Strength and +1 Charisma";
+      break;
+    case "Dwarf":
+      raceBonusInfo = "+2 Constitution";
+      break;
+    case "Elf":
+      raceBonusInfo = "+2 Dexterity";
+      break;
+    case "Gnome":
+      raceBonusInfo = "+2 Intelligence";
+      break;
+    case "Half-Elf":
+      raceBonusInfo = "+2 Charisma and +1 to Two Other Ability Scores of Your Choice";
+      break;
+    case "Halfling":
+      raceBonusInfo = "+2 Dexterity";
+      break;
+    case "Half-Orc":
+      raceBonusInfo = "+2 Strength and +1 Constitution";
+      break;
+    case "Human":
+      raceBonusInfo = "+1 to All Ability Scores";
+      break;
+    case "Tiefling":
+      raceBonusInfo = "+2 Charisma and +1 Intelligence";
+      break;
+  };
+
+  return (
+    <div>
+      <p>As a {props.class}, your suggested primary ability score is {firstSuggestion}, followed by {secondSuggestion}.</p>
+      <p>As a {props.race}, you will receive {raceBonusInfo}.</p>
+    </div>
+  );
+};
+
+export default StatsSuggestionBox;
