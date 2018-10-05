32072b8c6450852ceeca9d0747348dc4985fe2be
Blackout506 <blackout@Matthews-MacBook-Pro.local>
2018-10-04 22:46:32 -0400

    Add logic to grab data from classes json file in NewCharacterAbilities

--- src/containers/NewCharacter/AbilitiesAndSpellsLayout/NewCharacterAbilities/NewCharacterAbilities.js
+++ src/containers/NewCharacter/AbilitiesAndSpellsLayout/NewCharacterAbilities/NewCharacterAbilities.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import classes from "../../../../assets/classList/classes.json";
 
@@ -6,22 +7,41 @@ class NewCharacterAbilities extends Component {
 
   render () {
 
-    let barbarianAbilities = classes.Barbarian.abilities.base.map(ability => (
-      <div>
-        <ul>
-          <li>{ability.name} - {ability.description}</li>
-        </ul>
+    let playerAbilities = [];
+    for (let level in classes.Barbarian.abilities) {                //Loop through all levels in the classes' "abilities" section
+      classes.Barbarian.abilities[level].forEach((ability) => {     //Loop through all abilities in a given level
+        playerAbilities.push({                                      //Push the ability information as a new object to abilityHolder
+          id: ability.name,
+          name: ability.name,
+          description: ability.description,
+          level: ability.level
+        });
+      });
+    };
+
+    //Will be replaced with an ability card functional component
+    let form = playerAbilities.map(ability => (
+      <div key={ability.name}>
+        <h3>Name: {ability.name}</h3>
+        <h3>Description: {ability.description}</h3>
       </div>
     ));
 
     return (
       <div>
         <h3><strong>Your character will have the following abilities:</strong></h3>
-        {barbarianAbilities}
+        {form}
       </div>
     );
   }
 
 }
 
-export default NewCharacterAbilities;
+const mapStateToProps = (state) => {
+  return {
+    level: state.char.basicCharacterData.characterLevel,
+    class: state.char.basicCharacterData.characterClass
+  };
+};
+
+export default connect(mapStateToProps, null)(NewCharacterAbilities);
