782705cf0ee46b8ec7dcbd9feaacba3710a15b0b
Blackout506 <blackout@Matthews-MacBook-Pro.local>
2018-10-06 15:14:27 -0400

    Add check to NewCharacterAbilities to only show abilities up to the player's level

--- src/containers/NewCharacter/AbilitiesAndSpellsLayout/NewCharacterAbilities/NewCharacterAbilities.js
+++ src/containers/NewCharacter/AbilitiesAndSpellsLayout/NewCharacterAbilities/NewCharacterAbilities.js
@@ -12,41 +12,49 @@ class NewCharacterAbilities extends Component {
     if (this.props.class === "Barbarian") {
       for (let level in classes.Barbarian.abilities) {                //Loop through all levels in the classes' "abilities" section
         classes.Barbarian.abilities[level].forEach((ability) => {     //Perform action on all abilities in a given level
-          playerAbilities.push({                                      //Push the ability information as a new object to playerAbilities
-            id: ability.name,
-            name: ability.name,
-            description: ability.description,
-            level: ability.level
-          });
-        });
-      };
-    }
-    if (this.props.class === "Bard") {
-      for (let level in classes.Bard.abilities) {                //Loop through all levels in the classes' "abilities" section
-        classes.Bard.abilities[level].forEach((ability) => {     //Perform action on all abilities in a given level
-          playerAbilities.push({                                 //Push the ability information as a new object to playerAbilities
-            id: ability.name,
-            name: ability.name,
-            description: ability.description,
-            level: ability.level
-          });
-        });
-      };
-    }
-    if (this.props.class === "Cleric") {
-      for (let level in classes.Cleric.abilities) {                //Loop through all levels in the classes' "abilities" section
-        classes.Cleric.abilities[level].forEach((ability) => {     //Perform action on all abilities in a given level
-          playerAbilities.push({                                 //Push the ability information as a new object to playerAbilities
-            id: ability.name,
-            name: ability.name,
-            description: ability.description,
-            level: ability.level
-          });
+          if (ability.level <= this.props.level) {                    //Only act on abilities that apply to the player's chosen level
+            playerAbilities.push({                                    //Push the ability information as a new object to playerAbilities
+              id: ability.name,
+              name: ability.name,
+              description: ability.description,
+              level: ability.level
+            });
+          }
         });
       };
     }
 
-    //Will be replaced with an ability card functional component
+    if (this.props.class === "Bard") {
+      for (let level in classes.Bard.abilities) {                     //Loop through all levels in the classes' "abilities" section
+        classes.Bard.abilities[level].forEach((ability) => {          //Perform action on all abilities in a given level
+          if (ability.level <= this.props.level) {                    //Only act on abilities that apply to the player's chosen level
+            playerAbilities.push({                                    //Push the ability information as a new object to playerAbilities
+              id: ability.name,
+              name: ability.name,
+              description: ability.description,
+              level: ability.level
+            });
+          }
+        });
+      };
+    }
+
+    if (this.props.class === "Cleric") {
+      for (let level in classes.Cleric.abilities) {                   //Loop through all levels in the classes' "abilities" section
+        classes.Cleric.abilities[level].forEach((ability) => {        //Perform action on all abilities in a given level
+          if (ability.level <= this.props.level) {                    //Only act on abilities that apply to the player's chosen level
+            playerAbilities.push({                                    //Push the ability information as a new object to playerAbilities
+              id: ability.name,
+              name: ability.name,
+              description: ability.description,
+              level: ability.level
+            });
+          }
+        });
+      };
+    }
+
+    //Build the form with an ability card for each ability
     let form;
     if (playerAbilities.length !== 0) {
       form = playerAbilities.map(ability => (
@@ -63,7 +71,7 @@ class NewCharacterAbilities extends Component {
 
     return (
       <div>
-        <h3><strong>Your character will have the following abilities:</strong></h3>
+        <h3><strong>Your character will start with the following abilities:</strong></h3>
         {form}
       </div>
     );
