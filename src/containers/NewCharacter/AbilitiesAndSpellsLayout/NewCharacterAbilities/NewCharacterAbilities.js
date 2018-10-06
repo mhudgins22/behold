338afedde121df70e9f9047d9e547249d888655c
Blackout506 <blackout@Matthews-MacBook-Pro.local>
2018-10-06 17:59:39 -0400

    Add druid abilities to classes.json

--- src/containers/NewCharacter/AbilitiesAndSpellsLayout/NewCharacterAbilities/NewCharacterAbilities.js
+++ src/containers/NewCharacter/AbilitiesAndSpellsLayout/NewCharacterAbilities/NewCharacterAbilities.js
@@ -69,11 +69,27 @@ class NewCharacterAbilities extends Component {
       };
     }
 
+    if (this.props.class === "Fighter") {
+      for (let level in classes.Fighter.abilities) {                   //Loop through all levels in the classes' "abilities" section
+        classes.Fighter.abilities[level].forEach((ability) => {        //Perform action on all abilities in a given level
+          if (ability.level <= this.props.level) {                    //Only act on abilities that apply to the player's chosen level
+            playerAbilities.push({                                    //Push the ability information as a new object to playerAbilities
+              id: ability.name,
+              name: ability.name,
+              description: ability.description,
+              level: ability.level
+            });
+          }
+        });
+      };
+    }
+
     //Build the form with an ability card for each ability
     let form;
     if (playerAbilities.length !== 0) {
       form = playerAbilities.map(ability => (
         <Card
+        key={ability.name}
         cardType = "abilityCard"
         name = {ability.name}
         description = {ability.description}
